6d16c22add87b0181c18e42f1284b5604862972e
carcarvroom <carly_l@outlook.com>
2020-10-30 15:24:59 -0700

    updated component structure

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Login from "./layouts/Login.js"
 import indexRoutes from "./routes/index.jsx";
 import { Route, Switch } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "./assets/scss/style.scss";
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <Switch>
+      <Route exact path='/' key='home' component={Login} />
       {indexRoutes.map((prop, key) => {
         return <Route path={prop.path} key={key} component={prop.component} />;
       })}
     </Switch>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
---
+++ src/layouts/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <header className="topbar navbarbg" data-navbarbg="skin5">
+      Login
+    </header>
+  );
+};
+export default Login;
