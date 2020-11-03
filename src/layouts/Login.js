eb653f7907d764c25890a588ef1eefe0f5d82d5a
sfespinosa <scottjames.espinosa@gmail.com>
2020-11-02 18:04:28 -0800

    minor edits to login

--- src/layouts/Login.js
+++ src/layouts/Login.js
@@ -55,14 +55,14 @@ const Login = () => {
         <div className="overlay-container">
             <div className="overlay">
                 <div className="overlay-panel overlay-left">
-                    <h1>Welcome Back!</h1>
-                    <p>To keep connected with us please login with your personal info</p>
+                    <h1>Hello, Friend!</h1>
+                    <p>Enter your personal details and start your journey with us</p>
                     <button className="ghost" id="signIn" onClick={()=> setRightPanel(false)}>Sign In</button><br/>
                     <button className="ghost" id="signIn" onClick={()=> setProviderPage(!providerPage)}>Click here for {providerPage ? 'Patients' : 'Providers'}</button>
                 </div>
                 <div className="overlay-panel overlay-right">
-                    <h1>Hello, Friend!</h1>
-                    <p>Enter your personal details and start journey with us</p>
+                    <h1>Welcome Back!</h1>
+                    <p>To keep connected with us, please login with your personal credentials</p>
                     <button className="ghost" id="signUp" onClick={()=> setRightPanel(true)}>Sign Up</button><br/>
                     <button className="ghost" id="signIn" onClick={()=> setProviderPage(!providerPage)}>Click here for {providerPage ? 'Patients' : 'Providers'}</button>
                 </div>
