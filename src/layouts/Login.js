22471a395bcebf63d663475fd8511c1021f07e90
sfespinosa <scottjames.espinosa@gmail.com>
2020-11-03 11:09:29 -0800

    form control added

--- src/layouts/Login.js
+++ src/layouts/Login.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import '../assets/scss/login-signup.css'
 
 const Login = () => {
 
-  const [providerPage, setProviderPage] = React.useState(false)
-  const [rightPanel, setRightPanel] = React.useState(false)
+  // handles slide feature
+  const [providerPage, setProviderPage] = useState(false)
+  const [rightPanel, setRightPanel] = useState(false)
+
+  // handles login input changes
+  const [loginEmail, setLoginEmail] = useState('')
+  const [loginPassword, setLoginPassword] = useState('')
+
+  // handles signup input changes
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [signUpEmail, setSignUpEmail] = useState('')
+  const [organization, setOrganization] = useState('')
+  const [signUpPassword, setSignUpPassword] = useState('')
+  const [npiNumber, setNPINumber] = useState('')
+
 
   const renderPatientSignUp = () => {
     return (
       <form className='login-signin-form' action="#">
         <h2>Create Patient Account</h2>
-            <input className='login-signup-input' type="text" placeholder="First Name" />
-            <input className='login-signup-input' type="text" placeholder="Last Name" />
-            <input className='login-signup-input' type="email" placeholder="Email" />
-            <input className='login-signup-input' type="text" placeholder="Organization" />
-            <input className='login-signup-input' type="password" placeholder="Password" /><br/>
+            <input className='login-signup-input' type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)}/>
+            <input className='login-signup-input' type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)}/>
+            <input className='login-signup-input' type="email" placeholder="Email" onChange={(e) => setSignUpEmail(e.target.value)}/>
+            <input className='login-signup-input' type="text" placeholder="Organization" onChange={(e) => setOrganization(e.target.value)}/>
+            <input className='login-signup-input' type="password" placeholder="Password" onChange={(e) => setSignUpPassword(e.target.value)}/><br/>
             <button className='login-signin-button'>Sign Up</button>
       </form>
     )
@@ -24,13 +38,13 @@ const Login = () => {
     return (
       <form className='login-signin-form' action="#">
         <h2>Create Provider Account</h2>
-              <input className='login-signup-input' type="text" placeholder="First Name" />
-              <input className='login-signup-input' type="text" placeholder="Last Name" />
-              <input className='login-signup-input' type="email" placeholder="Email" />
-              <input className='login-signup-input' type="text" placeholder="Organization" />
-              <input className='login-signup-input' type="text" placeholder="Individual NPI Number" />
-              <input className='login-signup-input' type="password" placeholder="Password" /><br/>
-              <button className='login-signin-button'>Sign Up</button>
+        <input className='login-signup-input' type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)}/>
+            <input className='login-signup-input' type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)}/>
+            <input className='login-signup-input' type="email" placeholder="Email" onChange={(e) => setSignUpEmail(e.target.value)}/>
+            <input className='login-signup-input' type="text" placeholder="Organization" onChange={(e) => setOrganization(e.target.value)}/>
+            <input className='login-signup-input' type="password" placeholder="Password" onChange={(e) => setSignUpPassword(e.target.value)}/>
+            <input className='login-signup-input' type="number" placeholder="NPI Number" onChange={(e) => setNPINumber(e.target.value)}/><br/>
+            <button className='login-signin-button'>Sign Up</button>
       </form>
     )
   }
@@ -46,8 +60,8 @@ const Login = () => {
         <div className="form-container sign-in-container">
             <form className='login-signin-form' action="#">
                 <h2>{providerPage ? 'Provider' : 'Patient'} Sign in</h2>
-                <input className='login-signup-input' type="email" placeholder="Email" />
-                <input className='login-signup-input' type="password" placeholder="Password" />
+                <input className='login-signup-input' type="email" placeholder="Email" onChange={(e) => setLoginEmail(e.target.value)}/>
+                <input className='login-signup-input' type="password" placeholder="Password" onChange={(e) => setLoginPassword(e.target.value)}/>
                 <a className='a-link' href="#">Forgot your password?</a>
                 <button className='login-signin-button'>Sign In</button>
             </form>
