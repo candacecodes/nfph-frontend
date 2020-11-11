efe52574ef875c4430253eb6de2fac315025ae3b
iona-b <ionabrabender@gmail.com>
2020-11-11 11:13:33 -0800

    merge conflicts resolved

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-import { combineReducers } from "redux";
-import entryReducer from './entryReducer';
-
-const rootReducer = combineReducers({
-    entryReducer: entryReducer,
-})
-
-export default rootReducer;
-=======
 import { combineReducers } from 'redux';
 import organizationReducer from './organizationReducer';
 import patientReducer from './patientReducer';
+import entryReducer from './entryReducer';
 
 const rootReducer = combineReducers({
     patient: patientReducer,
-    organizations: organizationReducer
+    organizations: organizationReducer,
+    entryReducer: entryReducer,
 })
 
 export default rootReducer
->>>>>>> 9972dc34570d4ba91c81f7c549a143006a86a73b
