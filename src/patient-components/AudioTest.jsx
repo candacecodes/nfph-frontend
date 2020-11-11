355301b1aeeae147b3fec8b467585dfcec920cd3
candacecodes <candacehchoe@gmail.com>
2020-11-11 12:18:07 -0800

    added buttons to audio test

--- src/patient-components/AudioTest.jsx
+++ src/patient-components/AudioTest.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import {
+    Button,
+    ButtonGroup,
+    Card,
+    CardBody,
+    CardTitle,
+    Row,
+    Col
+} from 'reactstrap';
 
 export default class AudioTest extends Component {
     render() {
         return (
-            <div>
-                Audio Test 
+            <div> 
+                <center>
+                    <h1>Audio Test </h1>
+                    <br/>
+                    <h3>Please press button to hear sound</h3>
+                    <br/><br/><Button className="btn" color="primary">Sound Volume 1</Button> 
+                    <br/><br/><Button className="btn" color="primary">Sound Volume 2</Button> 
+                    <br/><br/><Button className="btn" color="primary">Sound Volume 3</Button> 
+                    <br/><br/><Button className="btn" color="primary">Sound Volume 4</Button> 
+</center>
             </div>
         )
     }
